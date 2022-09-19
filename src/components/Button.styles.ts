6c9329f6b851ba5ed753416408ccfe1328392ff2
leohb07 <leohb1501@gmail.com>
2022-09-19 16:39:39 -0300

    Criação do projeto, configuração do ESLint, styled components e react-router-dom

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  margin: 8px;
+  border: 0;
+  border-radius: 8px;
+  cursor: pointer;
+
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+`
